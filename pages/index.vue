3692b4c9f0dc0c0930bd7fc923b4c319743a6012
Joseledgy <jrl22ledger@gmail.com>
2021-06-26 07:22:24 +0300

    Added contact page.

--- pages/index.vue
+++ pages/index.vue
@@ -56,55 +56,44 @@
 						</v-card-text>
 					</v-col>
 					<v-col cols="12" md="6">
-						<v-img src="https://static.wixstatic.com/media/b69375_f7cfe407cc774592a88f28b62b12362b~mv2.jpg/v1/fill/w_630,h_420,al_c,lg_1,q_80/bg%20copy.webp"></v-img>
+						<v-img src="https://static.wixstatic.com/media/b69375_f7cfe407cc774592a88f28b62b12362b~mv2.jpg/v1/fill/w_630,h_420,al_c,lg_1,q_80/bg%20copy.webp" height="100%"></v-img>
 					</v-col>
 				</v-row>
 			</v-card>
 		</v-col>
 
-		<v-col cols="12">
-			<v-card>
+		<v-col cols="12" md="5" class="my-6">
+			<v-card flat tile>
 				<v-card-title class="headline">
-					Welcome to the Vuetify + Nuxt.js template
+					Contact Us
 				</v-card-title>
 				<v-card-text>
-					<p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-					<p>
-						For more information on Vuetify, check out the
-							<a
-							href="https://vuetifyjs.com"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							documentation
-						</a>.
-					</p>
-					<p>
-						If you have questions, please join the official
-						<a
-							href="https://chat.vuetifyjs.com/"
-							target="_blank"
-							rel="noopener noreferrer"
-							title="chat"
-						>
-							discord
-						</a>.
-					</p>
-					<p>
-						Find a bug? Report it on the github
-						<a
-							href="https://github.com/vuetifyjs/vuetify/issues"
-							target="_blank"
-							rel="noopener noreferrer"
-							title="contribute"
-						>
-							issue board
-						</a>.
-					</p>
-					<p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-					<div class="text-xs-right">
-						<em><small>&mdash; John Leider</small></em>
-					</div>
+					<v-row no-gutters>
+						<v-col cols="12">
+							<v-text-field
+								label="Name"
+							></v-text-field>
+						</v-col>
+						<v-col cols="12">
+							<v-text-field
+								label="Email"
+							></v-text-field>
+						</v-col>
+						<v-col cols="12">
+							<v-text-field
+								label="Phone"
+							></v-text-field>
+						</v-col>
+						<v-col cols="12">
+							<v-textarea
+								label="Message"
+								autoGrow
+								dense
+								hide-details
+								rows="3"
+							></v-textarea>
+						</v-col>
+					</v-row>
 				</v-card-text>
 				<v-card-actions>
 					<v-spacer />
@@ -113,7 +102,7 @@
 						nuxt
 						to="/inspire"
 					>
-						Continue
+						Submit
 					</v-btn>
 				</v-card-actions>
 			</v-card>
