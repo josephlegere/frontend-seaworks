e625d5648f74b0a38cd9c175d016bb52d1690ecc
Joseledgy <jrl22ledger@gmail.com>
2021-06-28 17:05:20 +0300

    Add rouer in nuxt config for the git repository. Added code deploying to github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,11 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  // Router
+  router: {
+    base: '/frontend-seaworks/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - Seaworks',
